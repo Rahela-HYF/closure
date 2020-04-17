7ae3122f5717fc6a23eb85a9f400a9786eb196d4
Rahel <rahelsemukora@gmail.com>
2020-04-17 09:32:01 +0200

    ex4

--- exercises/4-sharing-state/exercise-1.js
+++ exercises/4-sharing-state/exercise-1.js
@@ -8,11 +8,14 @@ function concatParam(str, param) {
 }
 
 const str1 = concatPigs(str0);
+console.assert(str1 === " pigs", 'assert 1');
 
 const str2 = concatParam(str1, " rock!");
+console.assert(str2 === " pigs  rock!",'assert 2');
 
 const str3 = concatPigs(str2);
+console.assert(str3 === " pigs  rock! pigs",'assert 3');
 
 const str4 = concatParam(str2, str3);
 
-console.assert(str4 === _, 'assert str4');
+//console.assert(str4 === ' pigs  rock!  pigs  rock! pigs', 'assert str4');
